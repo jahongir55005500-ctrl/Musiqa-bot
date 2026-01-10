1074b41f76f007c9499669605de4fab4f546cadd
jahongir55005500-ctrl <jahongir55005500@gmail.com>
2026-01-10 10:50:33 +0500

    Update Music_bot.py

--- Music_bot.py
+++ Music_bot.py
@@ -4,7 +4,7 @@ import yt_dlp
 import os
 
 # YANGI TOKENNI SHU YERGA QO'YING
-TOKEN '8350288555:AAF9vnKGF50isbJLvKUcEWhXAxf5ZoRVEHA' 
+TOKEN = '8350288555:AAFoUPWjWDzSu5xp_0q8LJZ5XlZACmI0K7A'
 KANAL_1 = '@uz_kayfiyat_kliplar'
 KANAL_2 = '@waveoffeelings001'
 bot = telebot.TeleBot(TOKEN)
@@ -13,7 +13,8 @@ def check_sub(user_id):
     try:
         s1 = bot.get_chat_member(KANAL_1, user_id).status
         s2 = bot.get_chat_member(KANAL_2, user_id).status
-        return s1 in ['creator', 'administrator', 'member'] and s2 in ['creator', 'administrator', 'member']
+        ok = ['creator', 'administrator', 'member']
+        return (s1 in ok) and (s2 in ok)
     except: return True
 
 @bot.message_handler(commands=['start'])
@@ -25,7 +26,7 @@ def start(message):
         markup.add(types.InlineKeyboardButton("1-kanal", url=f"https://t.me/{KANAL_1[1:]}"))
         markup.add(types.InlineKeyboardButton("2-kanal", url=f"https://t.me/{KANAL_2[1:]}"))
         markup.add(types.InlineKeyboardButton("✅ Tekshirish", callback_data="check"))
-        bot.send_message(message.chat.id, "⚠️ Kanallarga a'zo bo'ling:", reply_markup=markup)
+        bot.send_message(message.chat.id, "⚠️ Obuna bo'ling:", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: True)
 def search(message):
@@ -44,7 +45,7 @@ def search(message):
         for i, r in enumerate(res):
             markup.add(types.InlineKeyboardButton(f"{i+1}. {r['title'][:30]}", callback_data=f"dl_{r['id']}"))
         bot.edit_message_text("👇 Tanlang:", message.chat.id, m.message_id, reply_markup=markup)
-    except: bot.edit_message_text("❌ Topilmadi.", message.chat.id, m.message_id)
+    except: bot.edit_message_text("❌ Topilmadi yoki YouTube blokladi.", message.chat.id, m.message_id)
 
 @bot.callback_query_handler(func=lambda c: c.data.startswith('dl_'))
 def dl(call):
@@ -59,11 +60,12 @@ def dl(call):
             bot.send_audio(call.message.chat.id, a, title=info.get('title'))
         if os.path.exists(file): os.remove(file)
         bot.delete_message(call.message.chat.id, call.message.message_id)
-    except: bot.send_message(call.message.chat.id, "❌ Xato.")
+    except: bot.send_message(call.message.chat.id, "❌ Yuklashda xato.")
 
 @bot.callback_query_handler(func=lambda c: c.data == "check")
 def h(c):
     if check_sub(c.from_user.id): bot.edit_message_text("✅ Marhamat:", c.message.chat.id, c.message.message_id)
-    else: bot.answer_callback_query(c.id, "❌ A'zo bo'lmadingiz!", show_alert=True)
+    else: bot.answer_callback_query(c.id, "❌ Obuna bo'lmadingiz!", show_alert=True)
 
 bot.infinity_polling()
+
